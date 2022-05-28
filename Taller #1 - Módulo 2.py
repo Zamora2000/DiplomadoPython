6a27ed97b4dcda188b4589aa4d80c49a2ea6722b
Zamora2000 <81378346+Zamora2000@users.noreply.github.com>
2022-05-28 14:22:09 -0500

    Add files via upload

---
+++ Taller #1 - Módulo 2.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 28 13:25:34 2022
+
+@author: Alvaro Zamora
+"""
+
+# Taller #1, segundo módulo
+
+#Declaramos las variables que vamos a usar
+
+a = 5 #input("ingrese el valor para la variable a") 
+b = 4 #input("ingrese el valor para la variable b")
+c = 5 #input("ingrese el valor para la variable c")
+d = 6 #input("ingrese el valor para la variable d")
+e = 7 #input("ingrese el valor para la variable e")
+f = 8 #input("ingrese el valor para la variable f")
+
+# Armamos nuestra ecuación
+
+ecu1 = a + (b/c) / d + (e/f)
+
+
+#------------------------------------------------------------------
+
+# EJERCICIO #2
+
+# Taller 2, segundo módulo
+
+# Declaramos nuestras variables
+
+a = 2 #input("ingrese el valor para la variable a") 
+b = 3 #input("ingrese el valor para la variable b")
+c = 4 #input("ingrese el valor para la variable c")
+d = 5 #input("ingrese el valor para la variable d")
+
+# Armamos nuestra ocasión
+ 
+ecu2 = a - (b / (c-d) )
+
+#---------------------------------------------------------------------
+
+# Tercera parte
+
+ecu1 = ecu2
+ecu2 = ecu1
+
+# Imprimimos nuestro resultado final para el usuario
+
+print("El resultado es: " + ecu1)
+
+
+# Imprimimos nuestro resultado final para el usuario
+
+print("El resultado es: " + ecu2)
